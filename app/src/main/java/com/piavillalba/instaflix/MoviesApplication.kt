b82a9e4e88a41fb3b8e068b3f61fb2da2bb4c28b
María Pía <mvillalba@condorlabs.io>
2021-02-06 10:21:43 -0500

    Initial commit

---
+++ app/src/main/java/com/piavillalba/instaflix/MoviesApplication.kt
@@ -0,0 +1,7 @@
+package com.piavillalba.instaflix
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MoviesApplication : Application()
---
+++ app/src/main/java/com/piavillalba/instaflix/NavigationActivity.kt
@@ -0,0 +1,23 @@
+package com.piavillalba.instaflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+
+        val navView: BottomNavigationView = findViewById(R.id.bn_navigation)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+    }
+
+}
