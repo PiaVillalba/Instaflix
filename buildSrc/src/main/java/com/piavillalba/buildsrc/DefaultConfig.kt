2468854666d6ef3ed7eabda2ce3d69b10b09d39b
María Pía <mvillalba@condorlabs.io>
2021-02-08 03:35:16 -0500

    implement Kotlin Grdle DSL

---
+++ buildSrc/src/main/java/com/piavillalba/buildsrc/DefaultConfig.kt
@@ -0,0 +1,10 @@
+package com.piavillalba.buildsrc
+
+object DefaultConfig {
+    const val sdkCompileVersion = 30
+    const val sdkBuildToolsVersion = "30.0.2"
+    const val sdkMinVersion = 21
+    const val sdkTargetVersion = 29
+
+}
+
---
+++ buildSrc/src/main/java/com/piavillalba/buildsrc/LibsVersions.kt
@@ -0,0 +1,36 @@
+package com.piavillalba.buildsrc
+
+object LibsVersions {
+
+    // Kotlin
+    const val kotlin_version = "1.4.10"
+
+    // AndroidX
+    const val appCompat_version = "1.2.0"
+    const val legacy_support_version = "1.0.0"
+    const val material_version = "1.2.1"
+    const val constraint_version = "2.0.4"
+    const val navigation_version = "2.3.2"
+
+    // UI
+    const val skeleton_version = "1.1.2"
+    const val shimmer_version = "2.1.0"
+    const val glide_version = "4.9.0"
+
+    // DI
+    const val hilt_version = "2.31-alpha"
+
+    // Core
+    const val coroutines_version = "1.4.2"
+
+    // Networking
+    const val retrofit_version = "2.9.0"
+    const val okhttp_version = "4.9.0"
+    const val gson_version = "2.8.6"
+
+    // Unit test
+    const val junit_version = "4.+"
+    const val mockk_version = "1.10.5"
+    const val testRules_version = "1.1.1"
+
+}
